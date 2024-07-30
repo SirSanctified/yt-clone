ca18451d76ff2af82250d39885d0c8d89e2c0bb9
SirSanctified <mambambopritchard@gmail.com>
2024-07-30 12:12:59 +0200

    feat: Improve video details component layout and styles
    This commit updates the layout and styles of the video-details component. The changes include:
    - Adding CSS classes for image aspect ratio and skeleton loading animation
    - Updating the styles for like and dislike buttons
    - Modifying the line-clamp behavior for expanded and collapsed text
    - Adding a CSS animation for skeleton loading effect
    
    These changes enhance the visual appearance and user experience of the video details page.

--- src/app/video-details/video-details.component.ts
+++ src/app/video-details/video-details.component.ts
@@ -1,12 +1,124 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  inject,
+  Input,
+  OnInit,
+  Pipe,
+  PipeTransform,
+} from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faClosedCaptioning,
+  faEllipsis,
+  faExpand,
+  faForwardStep,
+  faGear,
+  faObjectGroup,
+  faPanorama,
+  faPlay,
+  faPlayCircle,
+  faThumbsDown,
+  faThumbsUp,
+  faToggleOn,
+  faUserCircle,
+  faVolumeUp,
+} from '@fortawesome/free-solid-svg-icons';
+import { formatDistanceStrict } from 'date-fns';
+import { FormatNumberPipe } from '../home/home.component';
+import { type VideoResponse } from './../youtube-videos.service';
+import { YoutubeVideosService } from '../youtube-videos.service';
+
+@Pipe({
+  name: 'duration',
+  standalone: true,
+})
+export class DurationPipe implements PipeTransform {
+  transform(value: string | undefined): string {
+    // return in format hr:min:sec, value is in seconds
+    if (!value) {
+      return '00:00';
+    }
+
+    const hours = Math.floor(Number(value) / 3600);
+    const minutes = Math.floor((Number(value) % 3600) / 60);
+    const seconds = Math.floor(Number(value) % 60);
+    if (hours > 0) {
+      return `${hours.toString().padStart(2, '0')}:${minutes
+        .toString()
+        .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    }
+
+    return `${minutes.toString().padStart(2, '0')}:${seconds
+      .toString()
+      .padStart(2, '0')}`;
+  }
+}
+
+@Pipe({
+  name: 'timeFromNow',
+  standalone: true,
+})
+export class TimeFromNowPipe implements PipeTransform {
+  transform(value: string | undefined): string {
+    if (!value) {
+      return '';
+    }
+    return formatDistanceStrict(new Date(value), new Date(), {
+      addSuffix: true,
+    });
+  }
+}
 
 @Component({
   selector: 'app-video-details',
   standalone: true,
-  imports: [],
+  imports: [
+    FontAwesomeModule,
+    RouterLink,
+    DurationPipe,
+    FormatNumberPipe,
+    TimeFromNowPipe,
+  ],
   templateUrl: './video-details.component.html',
-  styleUrl: './video-details.component.css'
+  styleUrl: './video-details.component.css',
 })
-export class VideoDetailsComponent {
+export class VideoDetailsComponent implements OnInit {
+  @Input() videoId!: string;
+  route = inject(ActivatedRoute);
+  youtubeVideosService = inject(YoutubeVideosService);
+  videoData: VideoResponse | undefined;
+  expand = false;
 
+  toggleExpand() {
+    this.expand = !this.expand;
+  }
+
+  play = faPlay;
+  volume = faVolumeUp;
+  next = faForwardStep;
+  autoPlay = faToggleOn;
+  subtitles = faClosedCaptioning;
+  settings = faGear;
+  cinemaMode = faPanorama;
+  fullscreen = faExpand;
+  multiplayer = faObjectGroup;
+  like = faThumbsUp;
+  dislike = faThumbsDown;
+  more = faEllipsis;
+  user = faUserCircle;
+  videos = faPlayCircle;
+
+  constructor(youtubeVideoService: YoutubeVideosService) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.videoId = params['videoId'];
+    });
+
+    console.log(this.videoId);
+    this.youtubeVideosService.getVideo(this.videoId).then((data) => {
+      this.videoData = data;
+    });
+  }
 }
--- src/app/youtube-videos.service.ts
+++ src/app/youtube-videos.service.ts
@@ -27,6 +27,92 @@ export interface HomeVideoData {
   thumbnail: Thumbnail[];
   richThumbnail: RichThumbnail[];
 }
+export interface Subtitle {
+  languageName: string;
+  languageCode: string;
+  isTranslatable: boolean;
+  url: string;
+}
+
+export interface TranslationLanguage {
+  languageCode: string;
+  languageName: string;
+}
+
+export interface SubtitleResponse {
+  subtitles: Subtitle[];
+  format: string;
+  translationLanguages: TranslationLanguage[];
+}
+
+export interface Storyboard {
+  width: string;
+  height: string;
+  thumbsCount: string;
+  columns: string;
+  rows: string;
+  interval: string;
+  storyboardCount: number;
+  url: string[];
+}
+
+export interface ChannelThumbnail {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface VideoData {
+  type: string;
+  videoId: string;
+  title: string;
+  lengthText: string;
+  viewCount: string;
+  publishedTimeText: string;
+  thumbnail: Thumbnail[];
+  channelTitle: string;
+  channelId: string;
+  channelThumbnail: ChannelThumbnail[];
+}
+
+export interface RelatedVideo {
+  continuation: string;
+  data: VideoData[];
+}
+
+export interface VideoResponse {
+  id: string;
+  title: string;
+  lengthSeconds: string;
+  keywords: string[];
+  channelTitle: string;
+  channelId: string;
+  description: string;
+  thumbnail: Thumbnail[];
+  allowRatings: boolean;
+  viewCount: string;
+  isPrivate: boolean;
+  isUnpluggedCorpus: boolean;
+  isLiveContent: boolean;
+  isCrawlable: boolean;
+  isFamilySafe: boolean;
+  availableCountries: string[];
+  isUnlisted: boolean;
+  category: string;
+  publishDate: string;
+  uploadDate: string;
+  subtitles: Subtitle;
+  storyboards: Storyboard[];
+  superTitle?: any;
+  likeCount: string;
+  channelThumbnail: ChannelThumbnail[];
+  channelBadges: string[];
+  subscriberCountText: string;
+  subscriberCount: number;
+  commentCountText: string;
+  commentCount: number;
+  relatedVideos: RelatedVideo;
+}
 
 export interface HomeVideoResponse {
   filters: Filter[];
@@ -53,6 +139,13 @@ export class YoutubeVideosService {
     const data: HomeVideoResponse = await response.json();
     return data;
   }
-  getVideo() {}
+  async getVideo(id: string): Promise<VideoResponse> {
+    const response = await fetch(
+      `${this.baseUrl}video/info?id=${id}`,
+      this.options
+    );
+    const data: VideoResponse = await response.json();
+    return data;
+  }
   getVideoComments() {}
 }
